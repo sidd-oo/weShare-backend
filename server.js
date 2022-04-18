6aa8e50c6a62bda440e8fde168ab07958a19dfa8
sidd-oo <sahoosiddharth3@gmail.com>
2022-04-18 21:18:50 +0530

    wip: Download page

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
-const fileRoute = require('./routes/files')
+const fileRoute = require('./routes/files');
+const showRoutes = require("./routes/show"); 
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 
 connectDB();
 
+//Template engine
+app.set('views', path.join(__dirname,'/views'));
+app.set('view engine', 'ejs')
+
+//Routes
 app.use("/api/files", fileRoute);
+app.use("/files", showRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
