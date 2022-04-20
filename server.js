5cd35e263f1f0ff81091de486fd22313ed95575a
sidd-oo <sahoosiddharth3@gmail.com>
2022-04-20 21:33:24 +0530

    feat: download file endpoint

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const connectDB = require('./config/db');
 const fileRoute = require('./routes/files');
 const showRoutes = require("./routes/show"); 
+const downloadRoutes = require("./routes/download");
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const { download } = require('express/lib/response');
 
 connectDB();
 
@@ -17,6 +19,7 @@ app.use(express.static('public'));
 //Routes
 app.use("/api/files", fileRoute);
 app.use("/files", showRoutes);
+app.use("/files/download", downloadRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
